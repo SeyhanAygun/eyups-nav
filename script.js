7d02a25c42556a4c8fcda7d853dbdb092f87d83d
SeyhanAygun <96574826+SeyhanAygun@users.noreply.github.com>
2022-06-07 15:27:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let haksayısı=0;
+let rastgelesayısı=0;
+let oyna=Document.getElementById("oyna");
+let sıfırla=document.getElementById("sıfırla");
+ let sonuç=document.getElementById("sonuç");
+let tahmin sayısı=document.getElementById("tahmin sayısı giriniz");
+let sayıtahmimi=document.getElementById=("sayıtahmini");
+
+oyna.onlick= oyna;
+sıfırla.onlick=sıfırla
+
+
+console.log(çalıstı);
+
+function oyna(){
+    var oyna=document.getElementById(oyna);
+    var  sonuç= ("daha buyuk bir sayı giriniz");
+    document.getElementById()
+
+}
+
+function sıfırlama(){
+let sonuç=sonuç sayıları sıfırla;
+let rastgelesayısı=("sıfırla");
+
+}
+
+
+
+
+function sonuç(){
+    let rasrgelesayı=0<50 sonuç=("daha buyuk bir sayı giriniz");
+    let deneme=("10");
+    
+    
+}
+
+
+
+
+
+function rasrgelesayı(){
+    let rasrgelesayı=100<1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
